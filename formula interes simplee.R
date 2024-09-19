c2420bad4e4b6e1a1a5ee2369e39058a7701c438
Chbips <2015162b@umich.mx>
2024-09-19 12:40:31 -0600

    carga tarea v 1.0
    se sube la primera versión 1.0 de ínteres simple

---
+++ formula interes simplee.R
@@ -0,0 +1,12 @@
+# Se presentan las funciones de interes simple
+# Autor: Chris rey
+# V 1.0: 19 de septiembre del 2024.
+
+# Valor futuro con ínteres simple:
+
+ValorFinalSimple=function(VA,r,t){
+  xsalida=VA*(1+(r*t))
+              
+  return(xsalida)
+}
+
